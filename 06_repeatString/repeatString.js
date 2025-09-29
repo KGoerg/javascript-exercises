ff0d17dcf69df496e911e529f81760b329aea0bf
kgoerg <224106412+KGoerg@users.noreply.github.com>
2025-09-29 15:38:04 -0500

    Get base code up and running for further testing

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,34 @@
-const repeatString = function() {
+// The below both worked but I wanted to rework it into a for loop instead of the while loop I found online. On the second one, I wanted to continue working with i instead of doing the "while (num > 0)" bit.
 
-};
+// const repeatString = function(string, num) {
+//     let repeatedString = "";
+//     while (num > 0) {
+//         repeatedString += string;
+//         num--;
+//     }
+//     return repeatedString;
+// }
+
+// const repeatString = function(string, num) {
+//     let repeatedString = "";
+//     let i = 0;
+//     while (i < num) {
+//         repeatedString += string;
+//         i++;
+//     }
+//     return repeatedString;
+// }
+
+const repeatString = function(string, num) {
+    let repeatedString = "";
+    for (let i = 0; i < num; i++) {
+    if (i < num) 
+        repeatedString += string;
+    }
+    return repeatedString;
+}
+
+repeatString('hello', 10)
 
 // Do not edit below this line
 module.exports = repeatString;
