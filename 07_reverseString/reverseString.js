74003a9f18b11e135820e75d068b5850b54e11cc
kgoerg <224106412+KGoerg@users.noreply.github.com>
2025-09-30 13:22:19 -0500

    Create code to solve this exercise

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+let outputArray = string.split(""); // should generate [h] [e] [l] [l] [o]
+let reversedLettersArray = outputArray.reverse(""); // [should generate [o] [l] [l] [e] [h]
+let reversedWord = reversedLettersArray.join(""); //should concatenate olleh.
+return reversedWord;
 };
 
 // Do not edit below this line
