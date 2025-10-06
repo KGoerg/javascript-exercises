a34cbbdddf17de272cfb313f7b5469c02e8b9826
kgoerg <224106412+KGoerg@users.noreply.github.com>
2025-10-06 15:39:33 -0500

    Add code to convert Celsius and Fahrenheit

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,14 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  const celsiusConversion = (temp - 32) * (5 / 9);
+  //Round result by taking original output multiplied by 10 and rounding to nearest whole number, then dividing by 10 to get a decimal.
+  let rounded = Math.round(celsiusConversion * 10) / 10;
+  return rounded;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  const fahrenheitConversion = (temp * (9/5) + 32);
+  let rounded = Math.round(fahrenheitConversion * 10) / 10;
+  return rounded;
 };
 
 // Do not edit below this line
