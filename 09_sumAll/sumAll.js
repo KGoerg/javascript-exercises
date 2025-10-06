3a5062eaedce431246e96e0132f3e6c55ad317a2
kgoerg <224106412+KGoerg@users.noreply.github.com>
2025-10-06 10:58:58 -0500

    Add if statement to return error for negatives

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,8 +1,10 @@
 const sumAll = function(firstInt, secondInt) {
 const numbersInRange = [];
-    // if firstInt || if secondInt < 0 {
-    //     return "ERROR"
-    // };
+//If the first parameter or second parameter are negative, return "ERROR";
+if (firstInt < 0 || secondInt < 0) {
+    return "ERROR";
+}
+
     for (let i = firstInt; i <= secondInt; i++){
     numbersInRange.push(i);
     }
