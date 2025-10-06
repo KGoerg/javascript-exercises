7a4b6ac425cc1cba779a695b02c28dd5011fa70c
kgoerg <224106412+KGoerg@users.noreply.github.com>
2025-10-06 10:50:23 -0500

    Add loops to create array and add all numbers in array

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(firstInt, secondInt) {
+const numbersInRange = [];
+    // if firstInt || if secondInt < 0 {
+    //     return "ERROR"
+    // };
+    for (let i = firstInt; i <= secondInt; i++){
+    numbersInRange.push(i);
+    }
+    for (let i = firstInt; i >= secondInt; i--){
+    numbersInRange.push(i);
+    }
+const finalSum = numbersInRange.reduce((accumulator, currentValue) => accumulator + currentValue
+);
+return finalSum;
 };
 
 // Do not edit below this line
