d66f236e21c4d2cb92d4e290863c215b4838948f
kgoerg <224106412+KGoerg@users.noreply.github.com>
2025-10-06 15:42:26 -0500

    Remove separate return lines and add return to new last line

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,14 +1,12 @@
 const convertToCelsius = function(temp) {
   const celsiusConversion = (temp - 32) * (5 / 9);
   //Round result by taking original output multiplied by 10 and rounding to nearest whole number, then dividing by 10 to get a decimal.
-  let rounded = Math.round(celsiusConversion * 10) / 10;
-  return rounded;
+  return rounded = Math.round(celsiusConversion * 10) / 10;
 };
 
 const convertToFahrenheit = function(temp) {
   const fahrenheitConversion = (temp * (9/5) + 32);
-  let rounded = Math.round(fahrenheitConversion * 10) / 10;
-  return rounded;
+  return rounded = Math.round(fahrenheitConversion * 10) / 10;
 };
 
 // Do not edit below this line
