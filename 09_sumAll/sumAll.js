009d7ed6caf0b479eb6e5720c6ea06f7aa1b8e28
kgoerg <224106412+KGoerg@users.noreply.github.com>
2025-10-06 12:25:02 -0500

    Add more conditions to avoid "ERROR" and complete tests

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,16 +1,19 @@
 const sumAll = function(firstNum, secondNum) {
 const numbersInRange = [];
 //If the first parameter or second parameter are negative, return "ERROR";
-if (firstNum < 0 || secondNum < 0) {
-    return "ERROR";
-}
-// if (Number.isInteger(firstNum || secondNum))
+
+// If the first parameter or second parameter are not integers, return "ERROR". First, test if these are integers. If they are integers, continue; if not, return "ERROR"
+
+if (firstNum > 0 && secondNum > 0 && Number.isInteger(firstNum) && Number.isInteger(secondNum)) {
     for (let i = firstNum; i <= secondNum; i++){
     numbersInRange.push(i);
     }
     for (let i = firstNum; i >= secondNum; i--){
     numbersInRange.push(i);
     }
+} else {
+    return "ERROR";
+};    
 const finalSum = numbersInRange.reduce((accumulator, currentValue) => accumulator + currentValue
 );
 return finalSum;
