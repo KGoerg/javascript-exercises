0575ba27813b54bb129d15f717ed093b4b1f8edc
kgoerg <224106412+KGoerg@users.noreply.github.com>
2025-10-06 14:14:09 -0500

    Complete code and all tests

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,6 +1,18 @@
-const leapYears = function() {
+const leapYears = function(year) {
+//Check if the year is divisible by 4 with no remainder. If so, continue to check for next condition:
+//If divisible by 100, it's not a leap year. UNLESS it's ALSO divisibe by 400.
 
+const divisibleByFour = year % 4 === 0;
+const divisibleByOneHundred = year % 100 === 0;
+const divisibleByFourHundred = year % 400 === 0;
+
+if ((divisibleByFour && divisibleByOneHundred && divisibleByFourHundred) || (divisibleByFour && !divisibleByOneHundred)) {
+    return true;
+} else {
+    return false;
+}
 };
+
 
 // Do not edit below this line
 module.exports = leapYears;
