6428a8b5947b04a2aae47b2533d42c164fca1cdb
kgoerg <224106412+KGoerg@users.noreply.github.com>
2025-10-28 10:23:33 -0500

    Complete retry of this exercise

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,45 +1,15 @@
-// This doesn't work because return kills the loop:
-// const repeatString = function(string, num) {
-//     for (i = 0; i < num; i++) {
-//         return string;
-//     }
-// }
-
-// The below both worked but I wanted to rework it into a for loop instead of the while loop I found online. On the second one, I wanted to continue working with i instead of doing the "while (num > 0)" bit.
-
-// const repeatString = function(string, num) {
-//     let repeatedString = "";
-//     while (num > 0) {
-//         repeatedString += string;
-//         num--;
-//     }
-//     return repeatedString;
-// }
-
-// const repeatString = function(string, num) {
-//     let repeatedString = "";
-//     let i = 0;
-//     while (i < num) {
-//         repeatedString += string;
-//         i++;
-//     }
-//     return repeatedString;
-// }
-
 const repeatString = function(string, num) {
-    let repeatedString = "";
-    let error = "ERROR";
-    let i = 0;
-    if (i > num) {
-        return error;
-        }
-    for (; i < num; i++) {
-        repeatedString += string;
+    let repeatedWord = "";
+    if (num < 0) {
+        return "ERROR";
     }
-    return repeatedString;
+    for (let i = 0; i < num; i++) {
+        repeatedWord += string;
+    }
+    return repeatedWord; 
 }
 
-repeatString('goodbye', -10)
+console.log(repeatString("hey", 3));
 
 // Do not edit below this line
 module.exports = repeatString;
