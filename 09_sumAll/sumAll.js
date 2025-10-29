30033c4a6563fac59b76bd6f410480b7b04fa60d
kgoerg <224106412+KGoerg@users.noreply.github.com>
2025-10-29 13:59:21 -0500

    Pass first two tests

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,23 +1,18 @@
-const sumAll = function(firstNum, secondNum) {
-const numbersInRange = [];
-//If the first parameter or second parameter are negative, return "ERROR";
-
-// If the first parameter or second parameter are not integers, return "ERROR". First, test if these are integers. If they are integers, continue; if not, return "ERROR"
-
-if (firstNum > 0 && secondNum > 0 && Number.isInteger(firstNum) && Number.isInteger(secondNum)) {
-    for (let i = firstNum; i <= secondNum; i++){
-    numbersInRange.push(i);
+function sumAll(num1, num2) {
+    let arr = [];
+    if (num1 < num2) {
+        for (let i = num1; i <= num2; i++) {
+        arr.push(i);
+        }
     }
-    for (let i = firstNum; i >= secondNum; i--){
-    numbersInRange.push(i);
-    }
-} else {
-    return "ERROR";
-};    
-const finalSum = numbersInRange.reduce((accumulator, currentValue) => accumulator + currentValue
-);
-return finalSum;
-};
+    // if (num1) {
+    //     for (let i = num1; i <= num2; i--) {
+    //         arr.push(i);
+    //     }
+    // }
+    return arr.reduce((sum, currentItem) => sum + currentItem);
+    };
+    
 
 // Do not edit below this line
 module.exports = sumAll;
