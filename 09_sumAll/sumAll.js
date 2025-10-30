2fa2f5d00569e1fc634e647756d76d3650891c39
kgoerg <224106412+KGoerg@users.noreply.github.com>
2025-10-30 13:58:58 -0500

    Pass fourth test

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,15 +1,15 @@
 function sumAll(num1, num2) {
     let arr = [];
-    if (num1 < num2) {
-        for (let i = num1; i <= num2; i++) {
-        arr.push(i);
-        }
-    }
     if (num1 > num2) {
         for (let i = num1; i >= num2; i--) {
             arr.push(i);
-        }
-    }
+        } 
+    } else if (num1 < 0 || num2 < 0) {
+        return "ERROR";
+    } else
+        for (let i = num1; i <= num2; i++) {
+        arr.push(i);
+        }  
     return arr.reduce((sum, currentItem) => sum + currentItem);
     };
     
