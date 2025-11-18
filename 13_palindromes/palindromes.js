599a7f1df0a9714f0b1b16407db11b9a028ecd00
kgoerg <224106412+KGoerg@users.noreply.github.com>
2025-11-18 13:51:35 -0600

    Pass first test

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    // let originalString = string
+    // .split("");
+    // // .filter((character) => ["!", ".", ","]);
+    let reverseString = string.split("").reverse().join("");
 
+    if (string === reverseString) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
