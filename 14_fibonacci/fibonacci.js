ef1429a6139fefa0ca635ec8e360afa792e3e61b
kgoerg <224106412+KGoerg@users.noreply.github.com>
2025-11-21 13:24:14 -0600

    Create base fibonacci sequence function

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,6 +1,20 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+  let num1 = 0;
+  let num2 = 1;
+  let count;
+  
+  if (num === 0) {
+    return num1;
+  } else {
+    for (i = 0; i < num; i++) {
+      count = num1 + num2;
+      num1 = num2;
+    }
+    return count;
+  }
 };
+
+console.log(fibonacci(6))
 
 // Do not edit below this line
 module.exports = fibonacci;
